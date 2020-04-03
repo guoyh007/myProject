f6ac66249510a957d32c64ab400eeced01e0ad60
郭永恒 <754308574@qq.com>
2020-04-03 18:49:25 +0800

    增加文字说明

--- src/pages/myCss/index.js
+++ src/pages/myCss/index.js
@@ -2,14 +2,21 @@ import React, { Component } from 'react';
 import styles from './index.less';
 
 function name(params) {
-  return(
-    <div className={styles.circleContainer}>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-  </div>
-  )
+  return (
+    <>
+      <div className={styles.circleContainer}>
+        1/2圆: <div></div>
+        1/4圆: <div></div>
+        1/2圆:  <div></div>
+        1/4圆: <div></div>
+      </div>
+
+      BFC:
+      <div className={styles.container}>
+        <div className={styles.inner}></div>
+      </div>
+    </>
+  );
 }
 
 export default name;
